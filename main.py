dc544b6e8ef87b2e9e16be237f5bf2da34b07cf9
raven <st450017@gmail.com>
2024-02-17 20:44:15 +0800

    feature

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-from src.discordBot import DiscordBot
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
-dcBot = DiscordBot()
-dcBot.start()
-print("123456")
+# 取得環境設定
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+# intents
+intents = discord.Intents.default()
+intents.message_content = True
+# client
+client = commands.Bot(command_prefix='$', intents=intents)
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:  # 排除機器人本身的訊息
+		return
+	await client.process_commands(message)
+
+@client.command()
+async def hello(ctx, message: str=""):
+	await ctx.send(f"您好w {'您說了' + message if message else ''}")
+
+@commands.command()
+async def test(ctx):
+	await ctx.send('test')
+
+client.add_command(test)
+				
+client.run(DISCORD_TOKEN)
